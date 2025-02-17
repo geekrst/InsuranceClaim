bae89eface4d7f041ff93f1061aa4deb7faf2104
RajeshSingh <RajeshSingh@GEEKRST>
2025-02-17 13:55:58 +0530

    Add project files.

---
+++ InsuranceClaim.Server/Data/InsuranceClaimDBContext.cs
@@ -0,0 +1,11 @@
+﻿using InsuranceClaim.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceClaim.Server.Data
+{
+    public class InsuranceClaimDBContext: DbContext
+    {
+        public InsuranceClaimDBContext(DbContextOptions options) : base(options) { }
+        public DbSet<ReserveModel> ReserveModel { get; set; }
+    }
+}
---
+++ InsuranceClaim.Server/Models/ReserveModel.cs
@@ -0,0 +1,19 @@
+﻿namespace InsuranceClaim.Server.Models
+{
+    public class ReserveModel
+    {
+        public Guid Id { get; set; }
+        public long ReserveDamage { get; set; }
+        public long ReserveClaimantCost { get; set; }
+        public long ReserveDefenceCost { get; set; }
+        public long PaidDamage { get; set; }
+        public long PaidClaimantCost { get; set; }
+        public long PaidDefenceCost { get; set; }
+        public long IncurredDamage { get; set; }
+        public long IncurredClaimantCost { get; set; }
+        public long IncurredDefenceCost { get; set; }
+        public string Status { get; set; }
+        public DateTime StatusDate { get; set; }
+
+    }
+}
